2f05b661b68aee7e3c8621f09524f6df9e7da699
mm2016 <mathismnl@gmail.com>
2016-01-03 20:27:41 +0100

    reorder folder

---
+++ scripts/edit.bat
@@ -0,0 +1,2 @@
+cd ../site_web/
+hugo server --watch
--- scripts/push.bat
+++ scripts/push.bat
@@ -1,7 +1,9 @@
+cd ..
 git pull
 git add . --all
 git commit -m "automatic commit"
 git push -u origin
+cd site_web
 hugo
 cd public
 surge --domain mano-test.surge.sh
